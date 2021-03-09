9529cfc75e4de98b886b749322299c5849ad33f0
Mikasa-san <54586069+Mikasa-san@users.noreply.github.com>
2021-03-09 12:34:53 +0900

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-if [ "$EUID" -ne 0 ]; then
-echo Please run this script as root.
-else
-echo -e "\e[31mWelcome to Uncursus Install Script V1.1 (Stable) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
-echo "WARNING : IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
-echo "If you found bug pls create an issues in github ;)"
-echo "Enjoy :)"
 echo "Starting..."
 echo "Installing Dependency's For The Installer"
 apt update
@@ -14,7 +6,7 @@ apt install com.bingner.plutil -y
 apt install zsh -y
 apt install curl -y
 echo "Downloading And Executing Offical Procurus Script From Coolstar"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jayguar1206/Uncursus/new/procursus-deploy-u0.sh)"
+curl -fsSL https://raw.githubusercontent.com/jayguar1206/Uncursus/new/procursus-deploy-u0.sh
 echo "Downloading And Installing Offical Procurus Deb"
 rm -rf /User/Documents/Uncursus
 mkdir /User/Documents/Uncursus
